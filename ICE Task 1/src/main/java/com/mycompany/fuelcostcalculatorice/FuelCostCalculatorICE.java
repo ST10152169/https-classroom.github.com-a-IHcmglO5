8a58c78a9b527c956939b489fbae8d2c55e1b03b
Ryan Samuels <ryansamuels360@gmail.com>
2026-03-21 18:22:49 +0200

    ICE Task 1 Fuel Cost Calculator ST10152169 Repository link
    https://github.com/ST10152169/ICE-Task-1/blob/main/src/main/java/com/mycompany/fuelcostcalculatorice/FuelCostCalculatorICE.java

---
+++ ICE Task 1/src/main/java/com/mycompany/fuelcostcalculatorice/FuelCostCalculatorICE.java
@@ -0,0 +1,67 @@
+
+package com.mycompany.fuelcostcalculatorice;
+
+/**
+ *
+ * @ST10152169, Ryan Samuels
+ */
+import java.util.Scanner;   // Shaik(2026) rules that importing scanner allows for user to add input 
+
+import java.text.DecimalFormat; //Decimal format import taught by Lambda(2023)
+
+import javax.swing.JOptionPane; //JOption Pane (Farrel,2023,pg 27)
+
+public class FuelCostCalculatorICE {   // Shaik (2026) states that a public class serves as a blueprint for the program
+
+    public static void main(String[] args) {  // Shaik (2026) implies that this line is known as the method signiture for program
+       
+      //Main Method (Farrel,2023,pg 59)
+      String userInputs;  
+      double distance;
+      Scanner inputDevice = new Scanner (System.in);
+        System.out.print("Enter total trip distance (km): ");
+      distance = inputDevice.nextDouble();
+      double fuelEfficiency;
+        System.out.print("Enter Fuel efficiency (km/litre): ");
+      fuelEfficiency = inputDevice.nextDouble();
+      double fuelPrice;
+        System.out.print("Enter price per litre (R): ");
+      fuelPrice =inputDevice.nextDouble();
+       
+        System.out.println("Distance: " + distance +"km" + " ;fuel efficiency: " + fuelEfficiency +"km/litre " + ";fuel price per litre:R " + fuelPrice);      
+        
+        DecimalFormat fmt1 =new DecimalFormat("#.##");
+        
+       String fuelNeededCalculation;   //Arithmetic method for "fuelNeededCalculation" credited to (Farrel,2023)
+       
+        
+        double fuelNeeded;
+        double average;
+        average = distance/fuelEfficiency;
+        fuelNeeded=average;
+        System.out.print(fmt1.format(average) + " Litres" + " is how much fuel is needed");
+        
+       String TotalCostCalculation; // Arithmetic method for "TotalCostCalculation" credited to (Farrel,2023)        
+        
+        
+        double totalCost;
+        totalCost = fuelNeeded * fuelPrice;
+        
+        System.out.println(" and the total Cost for trip will then be " + "R"+ fmt1.format(totalCost));
+        
+        System.out.println("R" +fmt1.format(totalCost)); //(lambda,2023)
+        
+        JOptionPane.showMessageDialog(null,"Total Cost for Trip: R" + fmt1.format(totalCost)); //(Farrel,2023,pg27)
+    }       
+    
+}
+
+
+/*
+Reference List:
+1. Shaik,F., (2026),Emeris(distance learning).online lecture youtube lessons[Prog5121].nt[online via internal Virtual Learning(VLE),
+The IndepenThe Indepedant Institute of Education.Available at: <https://www.youtube.com/playlist?list=PLDFRY8oCw2wDf5cS-Jg1DQb_aLoNnjIwX>
+[Accessed Date 21 March 2026].
+2.Farrel, J.,2023.Java Programming Tenth Edition. Boston: Cengage.
+3.Logic Lambda.,(2023)Using the Java DecimalFormat Class to Format Floating Point Values.Youtube Video.Available at:<https://www.youtube.com/watch?v=WgT7T6B5l8E>
+*/
